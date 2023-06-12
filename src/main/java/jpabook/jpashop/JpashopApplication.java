ed80c36c10423721d94b34d32c7ab0dcacb646de
minseok <alstjr706@naver.com>
2023-06-12 14:43:40 +0900

    git : 테스트

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-
     public static void main(String[] args) {
+        // git test
         SpringApplication.run(JpashopApplication.class, args);
     }
-
 }
+
