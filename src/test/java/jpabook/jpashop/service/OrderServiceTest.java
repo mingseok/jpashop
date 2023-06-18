e08fe0914858ca33b48709d3cb5da473298603f2
minseok <alstjr706@naver.com>
2023-06-18 22:00:54 +0900

    feat : (JPA) : 주문 취소 테스트 코드 구현

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -82,7 +82,6 @@ public class OrderServiceTest {
 
         assertEquals("주문 취소시 상태는 CANCEL 이다.", OrderStatus.CANCEL, getOrder.getStatus());
         assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
-
     }
 
     private Member createMember() {
